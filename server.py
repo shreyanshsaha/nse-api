a9b23fb59c60866ce8dd96337c892294d1438fc4
Shreyansh <yanshrey@gmail.com>
2024-03-24 21:56:10 +0530

    Added wsgi

--- server.py
+++ server.py
@@ -39,4 +39,4 @@ def get_stock_info_from_cache(stock):
    
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True, port=7654)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
